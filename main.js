004adf76814bd5a8acbb84794562b62c3f782e6b
Pratham Pujari <71821987+Pratham340@users.noreply.github.com>
2021-01-02 23:58:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+var SpeechRecognition=window.webkitSpeechRecognition;
+
+var recognition=new SpeechRecognition();
+
+function start(){
+    document.getElementById("textbox").innerHTML="";
+    recognition.start();
+}
+
+recognition.onresult= function (event){
+    var Content=event.results[0][0].transcript;
+
+    document.getElementById("textbox").innerHTML=Content;
+    if(Content == "take my selfie"){
+       console.log("taking your selfie");
+       speak();
+    }
+}
+function speak(){
+    var synth= window.speechSynthesis;
+    speak_data="Taking your selfie in 5 seconds";
+    var utterThis= new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+
+    setTimeout(function()
+    {
+        take_snapshot();
+    }, 5000);
+}
+
+camera=document.getElementById("camera");
+
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:'png',
+    png_quality:100
+});
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
